c4f88da7771f5291b087fd92c4f5b52b3946850c
amansingh202 <hellraisertelevision@gmail.com>
2024-01-17 17:02:41 -0600

    arithmetic operations

---
+++ arithmeticOperations.py
@@ -0,0 +1,13 @@
+#Arithmetic operations
+
+print(10 + 3) #13
+print(10 - 3) #7
+print(10 * 3) #30
+print( 10 / 3) #3.33
+print(10 // 3) #3
+print(10 % 3) #1
+print(10 ** 3) #1000
+
+x = 10
+x += 3 #x = x+3 #Augmented assignment operator
+print(x)
