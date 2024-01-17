b19878c68eada7fbef8d47b6b660948c71a226a9
amansingh202 <hellraisertelevision@gmail.com>
2024-01-17 16:46:55 -0600

    formatted strings

---
+++ formattedStrings.py
@@ -0,0 +1,11 @@
+#formatted strings
+first = "Aman"
+
+last = "Kumar"
+
+#Aman [Kumar] is a Developer
+result = first + ' [' + last + ']' + ' is a developer'
+print(result)
+
+message = f'{first} [{last}] is a developer' #formatted strings using placeholder ie.. {}
+print(message)
