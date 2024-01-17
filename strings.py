7dd89808e0f42fd10456169bea7ad75946484689
amansingh202 <hellraisertelevision@gmail.com>
2024-01-17 16:42:25 -0600

    string python

---
+++ strings.py
@@ -0,0 +1,28 @@
+#strings in python
+course = "Python's course for Beginners"
+print(course)
+
+#triple quotes
+course1 = '''Hey this is my first mail
+Thanks to the support team
+and other ITM department as well
+'''
+print(course1)
+
+print(course[0])
+print(course[-1])
+print(course[-2])
+print(course[0:6])
+#print everything
+print(course[1:])
+print(course[:6])
+print(course[:])
+
+#copy of a variable
+copy = course[:] #copy would contain all the characters of course variable
+print(copy)
+
+#exercise
+name = "Jennifer"
+print(name[1:-1])
+
