3121bf26a44cc7618ae300e0c2974db663794321
amansingh202 <hellraisertelevision@gmail.com>
2024-01-17 16:56:13 -0600

    string methods in python

---
+++ stringMethods.py
@@ -0,0 +1,20 @@
+#string methods
+
+course = 'Python For The Beginners'
+print(len(course)) #length of the string
+
+#upper and lower case methods
+print(course.upper())
+print(course)
+print(course.lower())
+print(course)
+
+#finding a character
+print(course.find('o')) #this method is case sensitive
+print(course.find('The'))
+
+#replace
+print(course.replace('Beginners', "Intermediate"))
+
+print("Python" in course) #true
+print("python" in course) #false
