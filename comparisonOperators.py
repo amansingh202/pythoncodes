29cdf1c5be2518ce66d97ba3d9a41e1bcf8e6735
amansingh202 <hellraisertelevision@gmail.com>
2024-01-18 10:15:40 -0600

    comparison operators with exercise

--- comparisonOperators.py
+++ comparisonOperators.py
@@ -17,3 +17,16 @@ else:
     print(f"It's neither hot nor cold day at {temp} degree C")
 
 
+#Exercise
+"""If name is less than 3 characters long -> name must be 3 characters long
+otherwise if it's more than 50 characters long -> name can be a maximum of 50 characters
+otherwise -> name looks good"""
+
+name = input("Enter the Person's name: ")
+
+if len(name) < 3:
+    print(f"name must be atleat 3 characters long")
+elif len(name) > 50:
+    print("name can be a maximum of 50 characters")
+else:
+    print("name look's good!")
