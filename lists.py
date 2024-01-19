cb4757bcbba90ef453357c62dbb6f2b2a3218e99
amansingh202 <hellraisertelevision@gmail.com>
2024-01-19 15:38:41 -0600

    lists python code

---
+++ lists.py
@@ -0,0 +1,19 @@
+#lists
+
+names = ['John', 'Bob', 'Sarah', 'Mosh']
+
+print(names)#in lits format []
+
+#individual names
+
+print(names[2])
+print(names[-1])
+print(names[-2])
+
+#for range of index from 2 to 3 excluding 4
+print(names[2:4])
+
+#modifying an element
+
+names[1] = "Aman"
+print(names)
