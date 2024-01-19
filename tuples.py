4822e543c77c8133cd8945fb1a914e78d8d70060
amansingh202 <hellraisertelevision@gmail.com>
2024-01-19 16:22:27 -0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#Tuples
+
+#Tuple are immutable ie.. they can't be modified which is no new items can be added or removed
+#tuples remain constant
+
+numbers = (2,5,8,12)
+
+print(numbers[1])
+
+numbers[2] = 13
+print(numbers)
