90ce22ddd75dd6da3ddd799d1b83921d904d32e5
amansingh202 <hellraisertelevision@gmail.com>
2024-01-19 15:19:45 -0600

    for loops

---
+++ forLoop.py
@@ -0,0 +1,15 @@
+#for loop
+
+for item in ['Raj','Aman', 'Satyam']:
+    print(item)
+
+for char in "Python":
+    print(char)
+
+#range of numbers
+
+for i in range(10):
+    print(i)
+
+for j in range(5,10):
+    print(j)
