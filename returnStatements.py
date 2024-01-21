3ff26eb0dfa011336c4432295decd03b89df7a06
amansingh202 <hellraisertelevision@gmail.com>
2024-01-20 21:53:54 -0600

    return statements in function

---
+++ returnStatements.py
@@ -0,0 +1,14 @@
+#Return statements in python
+
+#function that returns values
+
+def square(number):
+    return number*number
+
+
+result = square(5)
+print(result)
+
+#we can print this value directly inside the print statement
+print(square(6))
+
