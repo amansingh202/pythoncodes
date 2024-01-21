e9185a13ec9eed42b7afe28f02a46ae51d3565e6
amansingh202 <hellraisertelevision@gmail.com>
2024-01-20 21:27:01 -0600

    functions in python

---
+++ functions.py
@@ -0,0 +1,12 @@
+#functions in python
+
+#breaking code into re-usable codes is function
+
+def greetUser():
+    print("Hi there")
+    print("welcome aboad")
+
+print("Start")
+
+greetUser()
+print('Finish')
