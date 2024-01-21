fee3005185dfe6bf35cd40512bce461fd9228370
amansingh202 <hellraisertelevision@gmail.com>
2024-01-20 21:32:04 -0600

    parameters in function

---
+++ parameters.py
@@ -0,0 +1,12 @@
+#function parameters in python
+
+print("Start")
+
+def greetUser(first_name, lastname):
+    print(f"Hi there {first_name} {lastname}")
+    print(f"Welcome {first_name} {lastname}")
+
+greetUser("Aman", "Kumar")
+greetUser("Sakshi", "Sharma")
+
+print("End")
