1729c9a4223d88c54959d21bfa67ed7734b80acc
amansingh202 <hellraisertelevision@gmail.com>
2024-01-22 03:10:25 -0600

    files directories in python like creating, removing and searching etc

--- filesDirectories.py
+++ filesDirectories.py
@@ -10,11 +10,21 @@ print(path.exists()) #to check whether a path named emails exits or not
 
 #to create a new directory
 
-print(path.mkdir())
+#print(path.mkdir())
 
 #the above line of code will create a new directory called emails
 
 path2 = Path("Airline")
 
-print(path2.mkdir()) #this will create directory callied Airline
+#print(path2.mkdir()) #this will create directory callied Airline
+
+#to remove a directory
+#print(path2.rmdir())
+#this will remove the airline directory
+
+
+#to check for all the files and directories in a given path
+path3 = Path()
+for file in path3.glob('*.py'):  #* means everything
+    print(file)
 
