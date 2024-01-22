91b2ef351ac03854b614c70a2ef61fc4ff821a51
amansingh202 <hellraisertelevision@gmail.com>
2024-01-22 02:51:49 -0600

    random exercise for two dice

---
+++ randomsExercise.py
@@ -0,0 +1,8 @@
+#Roll a dice and every time a dice is rolled print the value ie.. (1,1), (5,6) etc.
+
+import random
+
+num1 = random.randint(1,6)
+num2 = random.randint(1,6)
+
+print(f"({num1},{num2})")
