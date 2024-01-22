06993da0bde5cade2972e1442b9f278d97bdbba6
amansingh202 <hellraisertelevision@gmail.com>
2024-01-22 02:20:54 -0600

    converters module

---
+++ converters.py
@@ -0,0 +1,11 @@
+#python modules
+
+#file with some python code 
+
+#breaking code into multiple files, these files are modules
+
+def lbs_to_kg(weight):
+    return 0.45 * weight
+
+def kg_to_lbs(weight):
+    return weight / 0.45
