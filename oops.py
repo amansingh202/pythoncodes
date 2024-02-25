bd02ea427dd1499bbcd34e1294718091d878e895
amansingh202 <hellraisertelevision@gmail.com>
2024-02-24 21:07:40 -0600

    child class creation

--- oops.py
+++ oops.py
@@ -3,6 +3,7 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
 
     #__str()__ function for string representation
     def __str__(self):
@@ -11,6 +12,9 @@ class Person:
     #creating user defined functions
     def myfunc(self):
         print(f"Hello, the name and age are {self.name}, {self.age}")
+
+    def printname(self):
+        print(f"{self.name}, {self.age}")
 
 #creating object of this class
         
@@ -28,4 +32,23 @@ print(p1.age)
 
 #print(type(p1.age))
 p1.myfunc()
+
+#creating a child class
+
+# class student is inheriting the class Person
+class Student(Person):
+    #pass #when we don't want to add anything in the class
+
+    #the init function will overwrite the properties of parent class
+
+    #to keep the inheritance of the parents use Parents init definition
+
+    def __init__(self, name, age, year):
+        super().__init__(name, age)
+        self.graduationYear = year
+
+
+x = Student("Mike", 23, 2019)
+print(x.graduationYear)
+print(x.name + " "+str(x.age))
 
