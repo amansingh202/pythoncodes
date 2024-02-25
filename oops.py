f727f0066a7a2d4f1b102b317d58a1f043fae8a7
amansingh202 <hellraisertelevision@gmail.com>
2024-02-24 20:48:03 -0600

    user defined functions for objects

--- oops.py
+++ oops.py
@@ -7,6 +7,10 @@ class Person:
     #__str()__ function for string representation
     def __str__(self):
         return f"{self.name}({self.age})"
+    
+    #creating user defined functions
+    def myfunc(self):
+        print(f"Hello, the name and age are {self.name}, {self.age}")
 
 #creating object of this class
         
@@ -15,4 +19,5 @@ p1 =Person("Jack", 42)
 print(p1.name)
 print(p1.age)
 
-print(type(p1.age))
+#print(type(p1.age))
+p1.myfunc()
